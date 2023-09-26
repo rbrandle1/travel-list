7db62c3cdff2889302ef5f02db1f572cdf9c3610
Ryan Brandle <r.brandle@gmail.com>
2023-09-26 16:55:51 -0600

    hooked up footer percentage calculations and early return

--- src/App.js
+++ src/App.js
@@ -73,10 +73,26 @@ const Item = ({ item, onDeleteItems, onToggleItems }) => {
 	);
 };
 
-const Stats = () => {
+const Stats = ({ items }) => {
+	if (!items.length) {
+		return (
+			<div className='stats'>
+				<em>Start adding some items to your packing list 🚀</em>
+			</div>
+		);
+	}
+
+	const numItems = items.length;
+	const numPacked = items.filter((item) => item.packed).length;
+	const percentage = Math.round((numPacked / numItems) * 100);
+
 	return (
 		<footer className='stats'>
-			<em>💼 You have X items on your list, and you already packed X (X%)</em>
+			<em>
+				{percentage !== 100
+					? `💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage || 0}%)`
+					: `You got everything! Ready to ✈️`}
+			</em>
 		</footer>
 	);
 };
@@ -101,7 +117,7 @@ const App = () => {
 			<Logo />
 			<Form onHandleItems={handleAddItems} />
 			<PackingList items={items} onDeleteItems={handleDeleteItems} onToggleItems={handleToggleItems} />
-			<Stats />
+			<Stats items={items} />
 		</div>
 	);
 };
