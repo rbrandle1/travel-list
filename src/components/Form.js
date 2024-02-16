b635e4bc0a6691ccace11ab77871280614d37a50
Ryan Brandle <r.brandle@gmail.com>
2024-02-16 16:22:53 -0700

    reverted test

--- src/components/Form.js
+++ src/components/Form.js
@@ -18,7 +18,7 @@ const Form = ({ onHandleItems }) => {
 
 	return (
 		<form className='add-form' onSubmit={handleSubmit}>
-			<h3>What do you need for your 😍 trip???</h3>
+			<h3>What do you need for your 😍 trip?</h3>
 			<select
 				value={quantity}
 				onChange={(e) => {
